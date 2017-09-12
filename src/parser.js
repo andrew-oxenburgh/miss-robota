486053ce944284d51c25cb2fc9f1cba42463fbe3
andrew <andrew.oxenburgh@gmail.com>
2017-09-12 12:56:11 +1200

    now is interactive

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,26 @@
 var fs = require('fs');
 var readline = require('readline');
 var { TableTop } = require('./robota');
-var argv = require('minimist')(process.argv.slice(2), { alias: { filename: '-f' } });
+var repl = require('repl');
+
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        filename: '-f', interactive: '-i',
+    }, });
 
 var table = new TableTop(argv.width, argv.height);
 
-if (argv.filename) {
+if (argv.interactive) {
+    function initializeContext(context) {
+        context.table = table;
+    }
+
+    console.log("REMINDER: you'll need a PLACE command to start this.");
+
+    var r = repl.start({ prompt: '> ', eval: table.command.bind(table) });
+    initializeContext(r);
+
+}else if (argv.filename) {
     var rl = readline.createInterface({
         input: fs.createReadStream(argv.filename),
         output: process.stdout,
